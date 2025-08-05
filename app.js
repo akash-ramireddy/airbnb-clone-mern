44f84d85a1796acbf54d6622ff4ac6f88ad9549c
Akash Ramireddy <akash119er@gmail.com>
2025-08-05 23:31:36 +0530

    Restructured routes into separate files.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+
+const Listing = require("./models/listing.js");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {validateListing,validateReview} = require("./utils/validate.js");
-const Review = require("./models/review.js");
+const listings = require("./routes/listings.js");
+const reviews = require("./routes/reviews.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -43,70 +44,8 @@ app.get("/",wrapAsync(async (req,res)=>{
     res.render("listings/index.ejs",{allListings});
 }));
 
-//Index Route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-
-//New Route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-});
-
-//Show Route
-app.get("/listings/:id",wrapAsync(async (req,res,next)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}));
-
-//Create Route
-app.post("/listings",validateListing,wrapAsync(async (req,res)=>{
-    const newListing=new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-}));
-
-//Edit Route
-app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-
-//Update Route
-app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-//Delete Route
-app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
-
-//Reviews
-//Post Route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async (req,res)=>{
-    let review=new Review(req.body.review);
-    let listing=await Listing.findById(req.params.id);
-    listing.reviews.push(review);
-    await review.save();
-    let result=await listing.save();
-    res.redirect(`/listings/${req.params.id}`);
-}));
-
-//Delete Review Route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async (req,res)=>{
-    let {id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-}));
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 app.use((req, res, next) => {
     return next(new ExpressError(404, "Page Not Found."));
---
+++ routes/reviews.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router({mergeParams:true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const {validateReview} = require("../utils/validate.js");
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+
+//Post Route
+router.post("/",validateReview,wrapAsync(async (req,res)=>{
+    let review=new Review(req.body.review);
+    let listing=await Listing.findById(req.params.id);
+    listing.reviews.push(review);
+    await review.save();
+    await listing.save();
+    res.redirect(`/listings/${req.params.id}`);
+}));
+
+//Delete Review Route
+router.delete("/:reviewId",wrapAsync(async (req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
+module.exports = router;
