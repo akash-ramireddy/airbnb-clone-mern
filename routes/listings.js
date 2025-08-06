918f16fa978466d85e9cf7de4403ff9de9bb4ada
Akash Ramireddy <akash119er@gmail.com>
2025-08-07 00:23:23 +0530

    Fixed server crashing when searched  for existing listing

--- routes/listings.js
+++ routes/listings.js
@@ -21,7 +21,7 @@ router.get("/:id",wrapAsync(async (req,res,next)=>{
     const listing=await Listing.findById(id).populate("reviews");
     if(!listing){
         req.flash("error","Listing you requested for does not exist!");
-        res.redirect("/listings");
+        return res.redirect("/listings");
     }
     res.render("listings/show.ejs",{listing});
 }));
@@ -40,7 +40,7 @@ router.get("/:id/edit",wrapAsync(async (req,res)=>{
     const listing=await Listing.findById(id);
     if(!listing){
         req.flash("error","Listing you requested for does not exist!");
-        res.redirect("/listings");
+        return res.redirect("/listings");
     }
     res.render("listings/edit.ejs",{listing});
 }));
